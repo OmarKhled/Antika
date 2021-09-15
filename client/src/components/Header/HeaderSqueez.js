1acbf47bf34e221a22524cd4a899020db4f5639a
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-09-15 21:33:19 +0200

    Header Adjustment

--- client/src/components/Header/HeaderSqueez.js
+++ client/src/components/Header/HeaderSqueez.js
@@ -5,7 +5,7 @@ const HeaderSqueez = () => {
     const header = document.querySelector("header");
     window.addEventListener("scroll", () => {
       // if (document.body.clientWidth > 575) {
-      if (window.scrollY > 33) {
+      if (window.scrollY > 20) {
         if (!header.classList.contains("squeez")) {
           header.classList.add("squeez");
         }
