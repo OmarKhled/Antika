0dd9314df31b0039b0f049f80f8d79bdfda07755
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-09-15 21:19:02 +0200

    Header Adjustment

--- client/src/components/Header/HeaderSqueez.js
+++ client/src/components/Header/HeaderSqueez.js
@@ -4,17 +4,17 @@ const HeaderSqueez = () => {
   useEffect(() => {
     const header = document.querySelector("header");
     window.addEventListener("scroll", () => {
-      if (document.body.clientWidth > 575) {
-        if (window.scrollY > 33) {
-          if (!header.classList.contains("squeez")) {
-            header.classList.add("squeez");
-          }
-        } else {
-          if (header.classList.contains("squeez")) {
-            header.classList.remove("squeez");
-          }
+      // if (document.body.clientWidth > 575) {
+      if (window.scrollY > 33) {
+        if (!header.classList.contains("squeez")) {
+          header.classList.add("squeez");
+        }
+      } else {
+        if (header.classList.contains("squeez")) {
+          header.classList.remove("squeez");
         }
       }
+      // }
     });
   }, []);
   return <></>;
